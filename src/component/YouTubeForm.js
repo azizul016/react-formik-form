a60f9a30791812b71976c17843a67e36264cceb3
azizul016 <haqueazizul789@gmail.com>
2021-04-17 01:57:06 +0600

    some practice done

--- src/component/YouTubeForm.js
+++ src/component/YouTubeForm.js
@@ -1,5 +1,18 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+  FieldArray,
+  FastField,
+} from "formik";
+// details in formik
+//Form is using for removing handleSubmit from form field
+//Field using for removing input field and manage values, handleChange and handleBlur
+// FastField converting Field and  using for render reducing
+//ErrorMessage using for showing error and pass a name value
+
 import * as Yup from "yup";
 import TextError from "./TextError";
 const initialValues = {
@@ -14,6 +27,8 @@ const initialValues = {
     twitter: "",
   },
   phoneNumber: ["", ""],
+  //multiple phone number in an array
+  phNumbers: [""],
 };
 
 const onSubmit = (values) => {
@@ -21,9 +36,9 @@ const onSubmit = (values) => {
 };
 
 const validationSchema = Yup.object({
-  name: Yup.string().required("Required"),
-  email: Yup.string().email("Invalid email format").required("Required"),
-  channel: Yup.string().required("Required"),
+  // name: Yup.string().required("Required"),
+  // email: Yup.string().email("Invalid email format").required("Required"),
+  // channel: Yup.string().required("Required"),
 });
 
 function YouTubeForm() {
@@ -71,10 +86,12 @@ function YouTubeForm() {
         </div>
 
         {/* Adding Address with Props */}
+        {/* using FirstField for reducing render */}
         <div className='form-control'>
           <label htmlFor='address'>Address</label>
-          <Field name='address'>
+          <FastField name='address'>
             {(props) => {
+              console.log("props render");
               // console.log("Render props value", props);
               const { field, form, meta } = props;
               return (
@@ -86,27 +103,80 @@ function YouTubeForm() {
                 </div>
               );
             }}
-          </Field>
+          </FastField>
+          {/* <Field name='address'>
+            {(props) => {
+              console.log("props render");
+              // console.log("Render props value", props);
+              const { field, form, meta } = props;
+              return (
+                <div>
+                  <input type='text' id='address' {...field} />
+                  {meta?.touched && meta?.error ? (
+                    <div>{meta.error}</div>
+                  ) : null}
+                </div>
+              );
+            }}
+          </Field> */}
         </div>
 
         {/* adding social network such as facebook, twitter */}
         <div className='form-control'>
           <label htmlFor='facebook'>Facebook Profiles</label>
-          <Field id='facebook' name='social.facebook' />
+          <Field type='text' id='facebook' name='social.facebook' />
         </div>
         <div className='form-control'>
           <label htmlFor='twitter'>Twitter Profiles</label>
-          <Field id='twitter' name='social.twitter' />
+          <Field type='text' id='twitter' name='social.twitter' />
         </div>
 
         {/* adding phone number */}
         <div className='form-control'>
           <label htmlFor='primaryPhoneNo'>Primary Phone Number</label>
-          <Field id='primaryPhoneNo' name='phoneNumber[0]' />
+          <Field type='text' id='primaryPhoneNo' name='phoneNumber[0]' />
         </div>
         <div className='form-control'>
           <label htmlFor='secondaryPhoneNu'>Secondary Phone Number</label>
-          <Field id='secondaryPhoneNu' name='phoneNumber[1]' />
+          <Field type='text' id='secondaryPhoneNu' name='phoneNumber[1]' />
+        </div>
+
+        {/* multiple phone number added in an array */}
+        <div className='form-control'>
+          <label>List of Phone Number</label>
+          <FieldArray name='phNumbers'>
+            {(fieldArrayProps) => {
+              // console.log("fieldArrayProps", fieldArrayProps);
+              // const { form, push, remove } = fieldArrayProps;
+              // const { values } = form;
+              // const { phNumbers } = values;
+              //code reduce, using nested destructuring
+              const {
+                form: {
+                  values: { phNumbers },
+                },
+                push,
+                remove,
+              } = fieldArrayProps;
+              return (
+                <div>
+                  {phNumbers.map((phNumber, index) => (
+                    <div key={index}>
+                      <Field name={`phNumbers[${index}]`} />
+                      {index > 0 && (
+                        <button type='button' onClick={() => remove(index)}>
+                          -
+                        </button>
+                      )}
+                      <button type='button' onClick={() => push("")}>
+                        +
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              );
+            }}
+          </FieldArray>
         </div>
 
         <button type='submit'>Submit</button>
